f007ca937f5138ec8278074269e0d058b696d9d1
solitary-cloud <1946306647@qq.com>
2022-12-13 13:27:06 +0800

    🎉 init(_): 项目搭建和屏幕适配

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from "vue-router";
+import useScalePage from "./hooks/useScalePage";
+
+useScalePage({
+  targetX: 1920,
+  targetY: 1080,
+  targetRatio: 16 / 9,
+});
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
